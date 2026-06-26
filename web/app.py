f4d681d50bcfc53da2de204d4532c16012c7e912
Michelle Granat <michelle.gra.frl@gmail.com>
2026-06-26 06:08:53 +0300

    Removed the front end

--- web/app.py
+++ web/app.py
@@ -1,24 +1,8 @@
-from pathlib import Path
+from flask import Flask
 
-from flask import Flask, render_template
-
-app = Flask(
-    __name__,
-    static_folder=str(Path(__file__).resolve().parent.parent / "public"),
-    static_url_path="/static",
-    template_folder="templates",
-)
+app = Flask(__name__)
 
 
 @app.route("/")
-def index():
-    return render_template("index.html")
-
-
-@app.route("/api/hello")
-def hello():
-    return "Hello from moni!"
-
-
-if __name__ == "__main__":
-    app.run(debug=True, port=3000)
+def hello_world():
+    return "<p>Hello, World!</p>"
