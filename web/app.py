37a2abb83498d71332c4df26043266f77d6d490f
Michelle Granat <michelle.gra.frl@gmail.com>
2026-06-26 05:22:48 +0300

    Added buttons to the main page

--- web/app.py
+++ web/app.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template
 app = Flask(
     __name__,
     static_folder=str(Path(__file__).resolve().parent.parent / "public"),
+    static_url_path="/static",
     template_folder="templates",
 )
 
